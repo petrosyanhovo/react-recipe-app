77464afa5e4a0f5166246eb689faf20b635baff2
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2022-10-29 15:08:40 +0400

    Added some styles

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import {Splide, SplideSlide} from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
 
 const Popular = () => {
 
@@ -14,19 +16,42 @@ const Popular = () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
     const data = await api.json();
     setPopular(data.recipes);
+    console.log(data.recipes);
   }
 
   return (
     <div>
         {popular.map((recipe) => {
             return (
-              <div>
-                  <p>{recipe.title}</p>
-              </div>
+              <Wrapper key={recipe.id}>
+                  <h3>Popular Picks</h3>
+                  {popular.map((recipe) => {
+                    return (
+                        <Card>
+                            <p>{recipe.title}</p>
+                            <img src={recipe.image} alt={recipe.title} />
+                        </Card>
+                    );
+                  })}
+              </Wrapper>
             )
         })}
     </div>
   )
 }
+
+const Wrapper = styled.div`
+    margin : 4rem 0rem;
+`;
+
+const Card = styled.div`
+    min-height : 25rem;
+    border-radius : 2rem;
+    overflow : hidden;
+
+    img {
+      border-radius : 2rem;
+    }
+`;
 
 export default Popular
